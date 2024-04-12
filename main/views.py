7015bc3cb782a3ff56c65c7d5ad0cc238ffda0fe
Xursandbek Tursunboev <khursanddd@gmail.com>
2024-04-12 20:31:26 +0500

    product detail

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
+from main import models
 
 
 class HomeView(View):
@@ -9,4 +10,14 @@ class HomeView(View):
             'title': 'Home page',
         }
         return render(request, 'index.html', context)
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = models.Product.objects.get(slug=slug)
+        context = {
+            'title': product.title,
+            'product': product,
+        }
+        return render(request, 'product_detail.html', context)
 
